40683f80cdf9e32a3e41e579c693be4daeb6da5b
divinejoshua <divineuche399@gmail.com>
2024-06-09 22:42:11 +0100

    Added client side routes

---
+++ src/app/(pages)/auth/google/page.tsx
@@ -0,0 +1,7 @@
+export default function GoogleAuthPage() {
+    return (
+      <main>
+        <h1>The Google Auth page</h1>
+      </main>
+    );
+  }
---
+++ src/app/(pages)/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function SettingsPage() {
+  return (
+    <main>
+      <h1>The settings page</h1>
+    </main>
+  );
+}
