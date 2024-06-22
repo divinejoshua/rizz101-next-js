2df3685a64b707aac74dfdd035aee110e1bd0c89
divinejoshua <divineuche399@gmail.com>
2024-06-22 16:40:09 +0100

    Edited init

--- src/app/hooks/usePaymentHook.tsx
+++ src/app/hooks/usePaymentHook.tsx
@@ -4,11 +4,9 @@ import { initAdmin } from "../firebase/firebaseAdmin";
 import { PAYMENTS_FIREBASE_TABLE } from "../constants/constants";
 
 const usePayment = () => {
-
-    initAdmin()
-
     //Save payment
     const saveTransactionEvent = async (paymentDetails : any) => {
+        initAdmin()
         // Get the `FieldValue` object
         var FieldValue = require("firebase-admin").firestore.FieldValue;
 
--- src/app/hooks/useUserHook.tsx
+++ src/app/hooks/useUserHook.tsx
@@ -6,10 +6,10 @@ import { USERS_FIREBASE_TABLE } from "../constants/constants";
 
 const useUser = () => {
 
-    initAdmin()
 
     // This is findUserById and Then create a new user if it doesn't exist
     const getOrCreateUser = async (user :IUser) =>{
+        initAdmin()
         if(!user.email) {throw("Email is missing")}
         let response = null
         const firestore = getFirestore()
@@ -33,6 +33,7 @@ const useUser = () => {
 
     //Create new user
     const createNewUser = async (user : IUser) => {
+        initAdmin()
         if(!user.email) {throw("Email is missing")}
 
         let response : any = {}
@@ -57,6 +58,7 @@ const useUser = () => {
 
     // This function only returns user details if user exist
     const getUserById = async (userId: string) =>{
+        initAdmin()
         if(!userId) {throw("UserId is missing")}
         let response = null
         const firestore = getFirestore()
@@ -76,6 +78,7 @@ const useUser = () => {
 
     // Function to update user's isSubscribed field to true
     const updateUserSubscription = async (email : string, subscribeValue : boolean) => {
+        initAdmin()
         if (!email) { throw ("Email is missing") }
         const firestore = getFirestore();
         const usersRef = firestore.collection(USERS_FIREBASE_TABLE);
