93b63c37b41c99580b94d8f7e45a0d8838bf5256
divinejoshua <divineuche399@gmail.com>
2024-06-23 19:03:22 +0100

    Updated payment link

--- src/app/(pages)/settings/done/page.tsx
+++ src/app/(pages)/settings/done/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function SuccessfulSubscription() {
   return (
-    <main className='mx-auto main-container mt-7 pl-4 pr-4 text-center mt-20'>Loading...</main>
+    <main className='mx-auto main-container mt-20 pl-4 pr-4 text-center mt-20'>Loading...</main>
   )
 }
--- src/app/(pages)/settings/page.tsx
+++ src/app/(pages)/settings/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import sliderImage from "@/app/assets/slider.png"
+import Link from "next/link";
 
 export default function SettingsPage() {
+
+  const PAYMENT_LINK = process.env.NEXT_PUBLIC_PAYMENT_LINK
   return (
     <main className="mx-auto main-container mt-7 pl-4 pr-4">
-      <div className="pb-4 mt-4">
+      <div className="pb-4 mt-20">
         <center>
           <Image src={sliderImage} alt="Rizz101" height={250}/>
         </center>
@@ -17,10 +20,14 @@ export default function SettingsPage() {
         <p>Premium</p>
         <p className="text-sm text-gray-500">From $5.99/Weekly</p>
       </div>
-      <button
-        className="bg-black w-full py-5 text-white font-bold rounded-full mt-5">
-          Get started
+
+      {/* Payment link */}
+      <Link href={`${PAYMENT_LINK}`} passHref={true}>
+        <button
+          className="bg-black w-full py-5 text-white font-bold rounded-full mt-5">
+            Get started
         </button>
+      </Link>
     </main>
   );
 }
